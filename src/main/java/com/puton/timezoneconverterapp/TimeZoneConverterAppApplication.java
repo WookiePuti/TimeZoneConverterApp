9bf7206d953e8a3fe6a576a18cefe16db1b8243a
Lukasz Puton <lukasputon@gmail.com>
2021-05-17 11:14:52 +0200

    Initial commit

---
+++ src/main/java/com/puton/timezoneconverterapp/TimeZoneConverterAppApplication.java
@@ -0,0 +1,13 @@
+package com.puton.timezoneconverterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeZoneConverterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeZoneConverterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puton/timezoneconverterapp/TimeZoneConverterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puton.timezoneconverterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeZoneConverterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
